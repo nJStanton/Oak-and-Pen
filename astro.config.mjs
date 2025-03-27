34a1c77e806cf899b396ad6c8a30bd4bf8fa4838
Nathaniel <nstanton1@sheffield.ac.uk>
2025-03-27 23:44:09 +0000

    Initial Landing Page design done.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+      plugins: [tailwindcss()],
+    },
+
+    //Activates image layout controls
+    image: {
+      // Used for all Markdown images; not configurable per-image
+      // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
+      experimentalLayout: 'responsive',
+    },
+    experimental: {
+      responsiveImages: true
+    },
+  });
