8b2ede8c1e7d7ae9209bb614eae3a8176f6f8738
Nathaniel <nstanton1@sheffield.ac.uk>
2025-03-28 00:00:24 +0000

    Added github pages information.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://nJStanton.github.io',
+    base: '/Oak-and-Pen',
     vite: {
       plugins: [tailwindcss()],
     },
